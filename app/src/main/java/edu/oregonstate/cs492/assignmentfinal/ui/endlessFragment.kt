f6c291fd533c576150cdb7eeb39f92cbf9c4162a
Cdttn8 <duttonch@oregonstate.edu>
2026-02-26 12:59:29 -0800

    added Pages and their Fragments
    added Pages and their Fragments setup the main page

---
+++ app/src/main/java/edu/oregonstate/cs492/assignmentfinal/ui/endlessFragment.kt
@@ -0,0 +1,12 @@
+package edu.oregonstate.cs492.assignmentfinal.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import edu.oregonstate.cs492.assignmentfinal.R
+
+class endlessFragment : Fragment(R.layout.endless_game_page) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/edu/oregonstate/cs492/assignmentfinal/ui/homeFragment.kt
@@ -0,0 +1,12 @@
+package edu.oregonstate.cs492.assignmentfinal.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import edu.oregonstate.cs492.assignmentfinal.R
+
+class homeFragment : Fragment(R.layout.home_page) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
