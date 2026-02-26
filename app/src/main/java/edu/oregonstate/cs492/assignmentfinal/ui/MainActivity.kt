7b80c02a43d23f26388c9ba06116bd59c3105540
Cdttn8 <duttonch@oregonstate.edu>
2026-02-26 12:21:57 -0800

    topappbar
    Added a base for the top app bar for upwards navigation need to add settings tab as well

--- app/src/main/java/edu/oregonstate/cs492/assignmentfinal/ui/MainActivity.kt
+++ app/src/main/java/edu/oregonstate/cs492/assignmentfinal/ui/MainActivity.kt
@@ -5,9 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 import edu.oregonstate.cs492.assignmentfinal.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfig: AppBarConfiguration
     private val tag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfig = AppBarConfiguration(navController.graph)
 
+        val topAppBar: MaterialToolbar = findViewById(R.id.top_app_bar)
+        setSupportActionBar(topAppBar)
+        setupActionBarWithNavController(navController, appBarConfig)
         }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+                || super.onSupportNavigateUp()
+    }
 }
